2a8a21c423236d81e1283b39d088c6556a473ac3
iFew <chitpong@gmail.com>
2018-01-18 23:34:35 +0700

    Change api from MVC to WebAPI

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -19,7 +19,6 @@ namespace api
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:5001")
                 .UseStartup<Startup>()
                 .Build();
     }
